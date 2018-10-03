ac30c6709bc9d44e3e194b0329e866d93e60249f
Alexander <arao81@gatech.edu>
2018-10-03 09:05:23 -0400

    Add basic React App

---
+++ src/Quideo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Quideo.css';
+
+class Quideo extends React.Component {
+    render() {
+        return (
+            <div className="quideo">
+                <h1>Hello, World!</h1>
+            </div>
+        );
+    }
+}
+
+export default Quideo;
