d2feaa91f2308b749b01c58c05734214d7aa5b47
Amarachi Nwakanma <anwakanma3@gatech.edu>
2018-10-23 15:08:05 -0400

    Update src/topic-sidebar.js

--- src/topic-sidebar.js
+++ src/topic-sidebar.js
@@ -15,6 +15,9 @@ class Sidebar extends React.Component{
 	//I will eventually be changing the state of the sidebar
 	constructor (props) {
 	super(props)
+	this.state = {
+		topics: [Week1;Week2;Week3;Week4;Week5;Week6;Week7;Week8;Week9;Week10;Week11;Week12;Week13]
+	}
 	}
 	}
 }
