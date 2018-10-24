1042b0f66041e9512ead699d291be0d5c7476dd3
Amanda <aschmitt8@gatech.edu>
2018-10-23 20:30:26 -0400

    Add text inside a table to help visualize format

--- src/Quideo.js
+++ src/Quideo.js
@@ -5,7 +5,30 @@ class Quideo extends React.Component {
     render() {
         return (
             <div className="quideo">
-                <h1>Hello, World!</h1>
+                <h1>Quideo</h1>
+                <p className="subtitle">Welcome to the vestion quank</p>
+                <table id="divider">
+                    <tr>
+                        <th>Learn</th>
+                        <th>Practice</th>
+                        <th>Test</th>
+                    </tr>
+                    <tr>
+                        <th>Here's a small rec guide</th>
+                        <th>mc question for this topic!</th>
+                        <th>short coding question for this topic</th>
+                    </tr>
+                    <tr>
+                        <th> video thumbnail</th>
+                        <th>tracing question</th>
+                        <th>short coding question for this topic</th>
+                    </tr>
+                    <tr>
+                        <th> video description/details</th>
+                        <th> first short coding</th>
+                        <th> harder long coding</th>
+                    </tr>
+                </table>
             </div>
         );
     }
