2ab31aaeaa76d5512336338693db24a7c1e976ee
Amarachi Nwakanma <anwakanma3@gatech.edu>
2018-10-26 21:49:31 -0400

    All the topic buttons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,14 +4,31 @@ import Button from 'react-bootstrap/lib/Button';
 import ButtonToolbar from 'react-bootstrap/lib/Button';
 //I wanna make all the buttons for the topics first
 class Side extends React.Component{
+
 	render () {
-	//When rendered I just want the topics to be displayed
+	//When rendered I just want the topics to be displayed as buttons but stacked
 	return (<ButtonToolbar>
-				<Button>Topic 1</Button>
-				<Button bsStyle="primary">Topic 2< /Button>
-	</ButtonToolbar>);
-	}
-}
+			<Button>Functions</Button>
+			<Button>Vectors</Button>
+			<Button>Strings</Button>
+			<Button>Arrays</Button>
+			<Button>Conditionals</Button>
+			<Button>Iteration</Button>
+			<Button>Low Level I/O</Button>
+			<Button>Cells</Button>
+			<Button>High Level I/O</Button>
+			<Button>Structures</Button>
+			<Button>Plotting</Button>
+			<Button>Numerical Methods</Button>
+			<Button>Recursion</Button>
+			<Button>Images</Button>
+			<Button>Sorting</Button>
+			<Button>Big O</Button>
+			<Button>Graph Theory</Button>
+			</ButtonToolbar>
+	);
+	};
+};
 
 // I want all of the buttons for questions to be rendered when the topic item is clicked ie a state change
 
@@ -19,14 +36,15 @@ class Sidebar extends React.Component{
 	//I will eventually be changing the state of the sidebar
 	constructor (props) {
 	super(props)
-	this.state = {
-		topics: ['Week1']
+	this.State = {
+		topics: ['Functions']
 	}
 	}
+
 	render() {
-		return <Side />;
-	}
-}
+	return <Side />
+	};
+};
 
 export default Sidebar
 
