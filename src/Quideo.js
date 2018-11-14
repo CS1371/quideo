4477c705c8355861799680de5479e3228b5ff320
Amanda <aschmitt8@gatech.edu>
2018-11-13 20:45:15 -0500

    add title

--- src/Quideo.js
+++ src/Quideo.js
@@ -4,22 +4,24 @@ import './Quideo.css';
 class Quideo extends React.Component {
     render() {
         return (
-            //<h1> Quideo! </h1>
             <div className="quideo">
-                <div className="learn">
-                    <h2> Learn </h2>
-                    <p> here is a rec guide </p>
-                    <p> here is a video thumbnail </p>
-                </div>
-                <div className="practice">
-                    <h2> Practice </h2>
-                    <p> here is a question! </p>
-                    <p> here is a question! </p>
-                </div>
-                <div className="test">
-                    <h2> Test Yourself </h2>
-                    <p> here is a question! </p>
-                    <p> here is a question! </p>
+                <h1> Quideo! </h1>
+                <div className="columns">
+                    <div className="learn">
+                        <h2> Learn </h2>
+                        <p> here is a rec guide </p>
+                        <p> here is a video thumbnail </p>
+                    </div>
+                    <div className="practice">
+                        <h2> Practice </h2>
+                        <p> here is a question! </p>
+                        <p> here is a question! </p>
+                    </div>
+                    <div className="test">
+                        <h2> Test Yourself </h2>
+                        <p> here is a question! </p>
+                        <p> here is a question! </p>
+                    </div>
                 </div>
             </div>
         );
