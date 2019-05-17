5a45b0df6cfbb1c850f6f54716856a4ed8300c9d
Alexander Rao <alexhrao@gmail.com>
2019-05-17 12:12:27 -0500

    Add editing and moving ability

--- src/question-editor/MultipleChoice.jsx
+++ src/question-editor/MultipleChoice.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import hash from 'object-hash';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimesSquare } from '@fortawesome/pro-solid-svg-icons';
+import {
+  faTimesSquare,
+  faChevronSquareUp,
+  faChevronSquareDown
+} from '@fortawesome/pro-solid-svg-icons';
 import { MultipleChoiceAnswer as AnswerType } from '../utility';
 import { Option } from '../question-viewer';
 import MarkdownEditor from './MarkdownEditor';
@@ -52,6 +56,39 @@ export default class MultipleChoice extends React.Component {
     const { value, onChange } = this.props;
     value.splice(i, 1);
     onChange(value);
+  };
+
+  swapChoice = (a, b) => {
+    const { value, onChange } = this.props;
+    // check bounds
+    if (a >= value.length || a < 0 || b >= value.length || b < 0) {
+      return;
+    }
+    [value[a], value[b]] = [value[b], value[a]];
+    onChange(value);
+  };
+
+  renderMovers = i => {
+    const { value } = this.props;
+    const moveModifier = `${i === 0 ? 'no-up' : ''} ${i === value.length - 1 ? 'no-down' : ''}`;
+    return (
+      <div className={`choice-movers ${moveModifier}`}>
+        <FontAwesomeIcon
+          key="move-up"
+          icon={faChevronSquareUp}
+          onClick={() => {
+            this.swapChoice(i, i - 1);
+          }}
+        />
+        <FontAwesomeIcon
+          key="move-down"
+          icon={faChevronSquareDown}
+          onClick={() => {
+            this.swapChoice(i, i + 1);
+          }}
+        />
+      </div>
+    );
   };
 
   renderEditor = () => {
@@ -108,27 +145,28 @@ export default class MultipleChoice extends React.Component {
   };
 
   render() {
-    const { value, onChange } = this.props;
+    const { value } = this.props;
     return (
       <div className="multipe-choice-container">
         <h2>Possible Choices</h2>
         <div className="confirmed-choices">
-          {value.length !== 0 ? <p>Click on a choice to delete it</p> : null}
+          {value.length !== 0 ? <p>Click on a choice to edit</p> : null}
           {value.map((c, i) => {
             // What if the user makes two identical answers? Alert if hash would be the same
             return (
               <div key={hash(c)} className="single-choice">
-                <FontAwesomeIcon
-                  icon={faTimesSquare}
-                  onClick={() => {
-                    this.removeChoice(i);
-                  }}
-                />
+                {this.renderMovers(i)}
                 <Option
                   {...c}
                   shouldExpand
                   handler={() => {
                     this.setState(c);
+                    this.removeChoice(i);
+                  }}
+                />
+                <FontAwesomeIcon
+                  icon={faTimesSquare}
+                  onClick={() => {
                     this.removeChoice(i);
                   }}
                 />
