770781f2dad857ee173e6e8ef6e733a0ec5b8650
Alexander Rao <alexhrao@gmail.com>
2019-05-29 22:34:11 -0500

    Add markdown instructions

--- src/question-editor/MarkdownEditor.jsx
+++ src/question-editor/MarkdownEditor.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AceEditor from 'react-ace';
 import Markdown from 'react-markdown';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuestionCircle } from '@fortawesome/pro-light-svg-icons';
 import { CodeBlock, Blank } from '../utility';
 
 import 'brace/mode/markdown';
@@ -9,6 +11,140 @@ import 'brace/theme/tomorrow_night_bright';
 
 import './MarkdownEditor.css';
 
+const MARKDOWN_GUIDE = `
+# Markdown Syntax
+
+Markdown is a simple markup language that makes it easy to apply formatting.
+
+## Headers
+
+To make headers, just use the \`#\` character, like so:
+
+\`\`\` markdown
+# Header
+## Small Header
+### Even Smaller Header
+\`\`\`
+
+Becomes
+
+# Header
+## Small Header
+### Even Smaller Header
+
+## Text
+
+Text will automatically be wrapped - long lines of text, or consecutive lines of text,
+will be shown as one flowing line. To insert a line break, just leave a blank line in between.
+
+Use double \`**asterisks**\` to make it **bold**, single \`_underscores_\` for _italics_,
+and double \`~~tildes~~\` for ~~strikethrough~~.
+
+## Blanks
+
+To insert blanks (for fill in the blank), use \`~~!Answer!~~\`. This can be used ~~!Anywhere!!~~
+
+In general, you should try to make your ~~!blanks!~~ less than 20 characters.
+Otherwise, it ~~!turns red, like this box is. You should keep it to fewer than 20 characters!!~~
+
+
+## Lists
+
+There are two types of lists - ordered and unordered.
+
+To do an unordered list, you can use \`*\` or \`-\`. For an ordered list, 
+you'll have to do that yourself (though it will be formatted). Make sure to include
+a space after the bullet (i.e., \`* bullet\`, not \`*bullet\`)
+
+\`\`\` markdown
+* Bullet 1
+* Bullet 2
+...
+- Bullet 1
+- Bullet 2
+...
+1. Order one
+2. Order two
+\`\`\`
+
+Becomes
+
+* Bullet 1
+* Bullet 2
+...
+- Bullet 1
+- Bullet 2
+...
+1. Order one
+2. Order two
+
+## Code
+
+To make something look like generic code, use backticks: \\\`I am code\\\`.
+
+To make a code _block_, use triple backticks.
+
+\`\`\`
+Hello! I am generic code!
+\`\`\`
+
+To make \`matlab\` code, use the triple backticks followed by the word \`matlab\`:
+
+\\\`\\\`\\\` matlab
+
+Code
+
+\\\`\\\`\\\`
+
+Will look like
+
+\`\`\` matlab
+function myFun(in1, in2, in3)
+    disp('hello, world!');
+end
+\`\`\`
+`;
+
+class MarkdownSyntax extends React.Component {
+  static propTypes = {
+    alwaysShow: PropTypes.bool
+  };
+
+  static defaultProps = {
+    alwaysShow: false
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isShown: false
+    };
+  }
+
+  render() {
+    const { isShown } = this.state;
+    const { alwaysShow } = this.props;
+    return (
+      <div className="markdown-syntax">
+        {alwaysShow ? null : (
+          <button
+            type="button"
+            className="markdown-syntax-btn"
+            onClick={() => this.setState({ isShown: !isShown })}
+          >
+            <FontAwesomeIcon icon={faQuestionCircle} />
+          </button>
+        )}
+        {!isShown && !alwaysShow ? null : (
+          <div className="markdown-preview">
+            <Markdown source={MARKDOWN_GUIDE} renderers={{ code: CodeBlock, delete: Blank }} />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 const MarkdownEditor = props => {
   const { value, title, help, onChange, hidePreview, height } = props;
   // if the type isn't specified, hidden!
@@ -16,6 +152,7 @@ const MarkdownEditor = props => {
     <div className={`markdown-container ${title === '' ? 'markdown-hidden' : 'markdown-shown'}`}>
       <h2>{title}</h2>
       <p>{help}</p>
+      <MarkdownSyntax />
       <div className="markdown-editor" title="I support Markdown!">
         <AceEditor
           mode="markdown"
