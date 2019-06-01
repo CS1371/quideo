2325ceee25ddf3f955c72af78e108662c85fd56d
Alexander Rao <alexhrao@gmail.com>
2019-06-01 14:14:36 -0500

    Refactor syntax to own file

--- src/utility/index.js
+++ src/utility/index.js
@@ -2,3 +2,4 @@ export { CodeBlock, codeLines } from './CodeBlock';
 export { MultipleChoiceAnswer, Tag, Question, TYPES } from './Properties';
 export { default as OrderedList } from './OrderedList';
 export { default as Blank } from './Blank';
+export { default as MarkdownSyntax } from './MarkdownSyntax';
